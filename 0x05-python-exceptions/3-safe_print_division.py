d1b71d7093d0b9c232e5fc6b080e3e40baf90e41
Kimani03 <victorkariuki03@gmail.com>
2023-03-27 13:31:09 -0700

    Tasks

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
     try:
-        result = a / b
-    except ZeroDivisionError:
-        print("Cannot divide by zero")
-        return None
+        res = a / b
+    except:
+        res = None
     finally:
-        print("Inside result: {}".format(result))
-    return result
+        print("Inside result: {}".format(res))
+        return res
