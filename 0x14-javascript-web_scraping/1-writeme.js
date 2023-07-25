55d25c2c12f1deed67eaf870585361d8bf320fde
Kimani03 <victorkariuki03@gmail.com>
2023-07-26 00:13:02 +0300

    a script that display the status code of a GET request.

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -3,7 +3,7 @@ const fs = require('s');
 const fileName = process.argv[2];
 const fileContent = process.argv[3];
 
-fs.writeFile(fileName, fileContent, 'utf-8', (err) => {
+fs.writeFile(fileName, content, 'utf-8', (err) => {
     if (err) {
         console.log(err);
     }
