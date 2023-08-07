5f419e5e5b4fefe5d21e1ce0deb846f0dbde4314
Kimani03 <victorkariuki03@gmail.com>
2023-08-07 12:31:39 +0300

    Changes

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -7,9 +7,9 @@ if __name__ == "__main__":
     if count == 1:
         print("0 arguments.")
     elif count == 2:
-        print("{:D} arguement:".format(count -1))
+        print("{:d} arguement:".format(count -1))
     else:
         print("{:d} arguments:".format(count -1))
 
     for i in range(1, count):
-        print("{:d}: {:s}".format(i, argv[i]))
+        print("{:d}: {:s}".format((i), argv[i]))
